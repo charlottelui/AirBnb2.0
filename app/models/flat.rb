b4fa3f63b39ff336540def6533a4ee21faf64a32
Charlotte Lui <charlotteytlui@gmail.com>
2020-05-24 22:12:17 +0300

    validates flat

---
+++ app/models/flat.rb
@@ -0,0 +1,9 @@
+class Flat < ApplicationRecord
+  belongs_to :user
+
+  validates :address, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :flat_type, inclusion: { in: ["entire place", "private room", "shared room"]}
+end
