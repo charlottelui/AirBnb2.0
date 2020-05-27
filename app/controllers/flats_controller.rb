92edfbe4782157972fa094545c52021108606b8f
Charlotte Lui <charlotteytlui@gmail.com>
2020-05-27 16:47:35 +0300

    basic controller set up for new and create

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -3,4 +3,28 @@ class FlatsController < ApplicationController
     @flat = Flat.find(params[:id])
     authorize @flat
   end
+
+  def new
+    @flat = Flat.new
+    authorize @flat
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.user = current_user
+
+    authorize @flat
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:title, :address, :price, :guest, :bedrooms, :bathrooms, :flat_type, :description, :amenities)
+  end
+
 end
