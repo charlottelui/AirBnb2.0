8e7a525ea504135dc68341c56f89ec112c121d11
Angel Shamieh <angellshamieh5@gmail.com>
2020-05-31 20:07:33 +0300

    Add the fn to the show page, still need to move the fn to js file and design the showmore/less. one more problem need to fix is how to add classes to @flat.description in ruby

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -7,6 +7,7 @@ class FlatsController < ApplicationController
     @forth_photo = @flat.photos[3]
     @fifth_photo = @flat.photos[4]
     @remaining_photos = @flat.photos[5..-1]
+    @des_count = @flat.description.split('').count
     authorize @flat
   end
 
